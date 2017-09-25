42a29932b1532d551fc22dedf426f030f8197fef
Fadly <mfadlyrahman016@gmail.com>
2017-09-25 18:26:54 +0700

    Versi 3

--- Original/PCD-Luxan001/PCD-Luxan001/Form1.Designer.cs
+++ Original/PCD-Luxan001/PCD-Luxan001/Form1.Designer.cs
@@ -32,28 +32,12 @@
             this.button_openImage = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.eucnose = new System.Windows.Forms.TextBox();
-            this.eucpipi = new System.Windows.Forms.TextBox();
-            this.eucUmouth = new System.Windows.Forms.TextBox();
             this.eucchin = new System.Windows.Forms.TextBox();
-            this.ketLEyeB = new System.Windows.Forms.TextBox();
-            this.ketLEye = new System.Windows.Forms.TextBox();
-            this.ketnose = new System.Windows.Forms.TextBox();
-            this.ketpipi = new System.Windows.Forms.TextBox();
-            this.ketUmouth = new System.Windows.Forms.TextBox();
             this.ketdagu = new System.Windows.Forms.TextBox();
-            this.ketREye = new System.Windows.Forms.TextBox();
-            this.ketREyeB = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.eucLmouth = new System.Windows.Forms.TextBox();
             this.ketLmouth = new System.Windows.Forms.TextBox();
-            this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.Rcheck = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.avgbox = new System.Windows.Forms.TextBox();
             this.label13 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
@@ -102,28 +86,6 @@
             this.label9.TabIndex = 7;
             this.label9.Text = "Nose";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(1059, 418);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(107, 25);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Left Cheek";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(1059, 329);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(96, 25);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Up mouth";
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
@@ -135,33 +97,6 @@
             this.label6.TabIndex = 10;
             this.label6.Text = "Chin";
             // 
-            // eucnose
-            // 
-            this.eucnose.Location = new System.Drawing.Point(1192, 289);
-            this.eucnose.Margin = new System.Windows.Forms.Padding(4);
-            this.eucnose.Name = "eucnose";
-            this.eucnose.Size = new System.Drawing.Size(99, 22);
-            this.eucnose.TabIndex = 12;
-            this.eucnose.Text = "0";
-            // 
-            // eucpipi
-            // 
-            this.eucpipi.Location = new System.Drawing.Point(1192, 421);
-            this.eucpipi.Margin = new System.Windows.Forms.Padding(4);
-            this.eucpipi.Name = "eucpipi";
-            this.eucpipi.Size = new System.Drawing.Size(99, 22);
-            this.eucpipi.TabIndex = 13;
-            this.eucpipi.Text = "0";
-            // 
-            // eucUmouth
-            // 
-            this.eucUmouth.Location = new System.Drawing.Point(1192, 331);
-            this.eucUmouth.Margin = new System.Windows.Forms.Padding(4);
-            this.eucUmouth.Name = "eucUmouth";
-            this.eucUmouth.Size = new System.Drawing.Size(99, 22);
-            this.eucUmouth.TabIndex = 14;
-            this.eucUmouth.Text = "0";
-            // 
             // eucchin
             // 
             this.eucchin.Location = new System.Drawing.Point(1192, 495);
@@ -171,46 +106,6 @@
             this.eucchin.TabIndex = 15;
             this.eucchin.Text = "0";
             // 
-            // ketLEyeB
-            // 
-            this.ketLEyeB.Location = new System.Drawing.Point(1316, 128);
-            this.ketLEyeB.Margin = new System.Windows.Forms.Padding(4);
-            this.ketLEyeB.Name = "ketLEyeB";
-            this.ketLEyeB.Size = new System.Drawing.Size(95, 22);
-            this.ketLEyeB.TabIndex = 16;
-            // 
-            // ketLEye
-            // 
-            this.ketLEye.Location = new System.Drawing.Point(1316, 209);
-            this.ketLEye.Margin = new System.Windows.Forms.Padding(4);
-            this.ketLEye.Name = "ketLEye";
-            this.ketLEye.Size = new System.Drawing.Size(95, 22);
-            this.ketLEye.TabIndex = 17;
-            // 
-            // ketnose
-            // 
-            this.ketnose.Location = new System.Drawing.Point(1316, 289);
-            this.ketnose.Margin = new System.Windows.Forms.Padding(4);
-            this.ketnose.Name = "ketnose";
-            this.ketnose.Size = new System.Drawing.Size(95, 22);
-            this.ketnose.TabIndex = 18;
-            // 
-            // ketpipi
-            // 
-            this.ketpipi.Location = new System.Drawing.Point(1316, 331);
-            this.ketpipi.Margin = new System.Windows.Forms.Padding(4);
-            this.ketpipi.Name = "ketpipi";
-            this.ketpipi.Size = new System.Drawing.Size(95, 22);
-            this.ketpipi.TabIndex = 19;
-            // 
-            // ketUmouth
-            // 
-            this.ketUmouth.Location = new System.Drawing.Point(1316, 418);
-            this.ketUmouth.Margin = new System.Windows.Forms.Padding(4);
-            this.ketUmouth.Name = "ketUmouth";
-            this.ketUmouth.Size = new System.Drawing.Size(95, 22);
-            this.ketUmouth.TabIndex = 20;
-            // 
             // ketdagu
             // 
             this.ketdagu.Location = new System.Drawing.Point(1316, 492);
@@ -219,42 +114,6 @@
             this.ketdagu.Size = new System.Drawing.Size(95, 22);
             this.ketdagu.TabIndex = 21;
             // 
-            // ketREye
-            // 
-            this.ketREye.Location = new System.Drawing.Point(1316, 250);
-            this.ketREye.Margin = new System.Windows.Forms.Padding(4);
-            this.ketREye.Name = "ketREye";
-            this.ketREye.Size = new System.Drawing.Size(95, 22);
-            this.ketREye.TabIndex = 24;
-            // 
-            // ketREyeB
-            // 
-            this.ketREyeB.Location = new System.Drawing.Point(1316, 167);
-            this.ketREyeB.Margin = new System.Windows.Forms.Padding(4);
-            this.ketREyeB.Name = "ketREyeB";
-            this.ketREyeB.Size = new System.Drawing.Size(95, 22);
-            this.ketREyeB.TabIndex = 27;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(1059, 374);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 25);
-            this.label3.TabIndex = 28;
-            this.label3.Text = "low mouth";
-            // 
-            // eucLmouth
-            // 
-            this.eucLmouth.Location = new System.Drawing.Point(1192, 377);
-            this.eucLmouth.Margin = new System.Windows.Forms.Padding(4);
-            this.eucLmouth.Name = "eucLmouth";
-            this.eucLmouth.Size = new System.Drawing.Size(99, 22);
-            this.eucLmouth.TabIndex = 29;
-            this.eucLmouth.Text = "0";
-            // 
             // ketLmouth
             // 
             this.ketLmouth.Location = new System.Drawing.Point(1316, 457);
@@ -262,17 +121,6 @@
             this.ketLmouth.Name = "ketLmouth";
             this.ketLmouth.Size = new System.Drawing.Size(95, 22);
             this.ketLmouth.TabIndex = 30;
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(1311, 90);
-            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(95, 25);
-            this.label11.TabIndex = 32;
-            this.label11.Text = "Condition";
             // 
             // label12
             // 
@@ -293,14 +141,6 @@
             this.Rcheck.Size = new System.Drawing.Size(99, 22);
             this.Rcheck.TabIndex = 34;
             this.Rcheck.Text = "0";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(1316, 377);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(95, 22);
-            this.textBox2.TabIndex = 35;
             // 
             // avgbox
             // 
@@ -329,28 +169,12 @@
             this.ClientSize = new System.Drawing.Size(1431, 738);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.avgbox);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.Rcheck);
             this.Controls.Add(this.label12);
-            this.Controls.Add(this.label11);
             this.Controls.Add(this.ketLmouth);
-            this.Controls.Add(this.eucLmouth);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.ketREyeB);
-            this.Controls.Add(this.ketREye);
             this.Controls.Add(this.ketdagu);
-            this.Controls.Add(this.ketUmouth);
-            this.Controls.Add(this.ketpipi);
-            this.Controls.Add(this.ketnose);
-            this.Controls.Add(this.ketLEye);
-            this.Controls.Add(this.ketLEyeB);
             this.Controls.Add(this.eucchin);
-            this.Controls.Add(this.eucUmouth);
-            this.Controls.Add(this.eucpipi);
-            this.Controls.Add(this.eucnose);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button_openImage);
@@ -372,28 +196,12 @@
         private System.Windows.Forms.Button button_openImage;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox eucnose;
-        private System.Windows.Forms.TextBox eucpipi;
-        private System.Windows.Forms.TextBox eucUmouth;
         private System.Windows.Forms.TextBox eucchin;
-        private System.Windows.Forms.TextBox ketLEyeB;
-        private System.Windows.Forms.TextBox ketLEye;
-        private System.Windows.Forms.TextBox ketnose;
-        private System.Windows.Forms.TextBox ketpipi;
-        private System.Windows.Forms.TextBox ketUmouth;
         private System.Windows.Forms.TextBox ketdagu;
-        private System.Windows.Forms.TextBox ketREye;
-        private System.Windows.Forms.TextBox ketREyeB;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox eucLmouth;
         private System.Windows.Forms.TextBox ketLmouth;
-        private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.TextBox Rcheck;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox avgbox;
         private System.Windows.Forms.Label label13;
     }
